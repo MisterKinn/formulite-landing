8a1a7777a417cf4eaab82920add24e597e78631a
kinn <kinn@kinn.kr>
2026-01-11 17:15:56 +0900

    Modify document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
     return (
-        <Html>
+        <Html lang="ko">
             <Head>
                 {/* TossPayments SDK v2 스크립트 강제 삽입 */}
                 <script src="https://js.tosspayments.com/v2/standard"></script>
